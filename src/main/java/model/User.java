6b0dd1c9a93774149ebe921a7bc1ec1a6f2636a7
mihaildemonteske <kotello44@gmail.com>
2026-04-25 16:40:25 +0300

    starting the deal

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,65 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
+@Table
+
 public class User {
+    @Id
+    long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long userId;
+    String firstName;
+    String lastName;
+    String email;
+
+    public User() {
+    }
+    public User(long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
